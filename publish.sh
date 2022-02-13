2c550c22c9bc5695582c6e4802c5e728d9692d2b
Aaron Faanes <dafrito@gmail.com>
2022-02-13 01:30:55 -0600

    Ensure patch number doesn't increment on yarn publish failures

--- publish.sh
+++ publish.sh
@@ -3,9 +3,12 @@
 run_number=$1
 node_version=$2
 
+PUBLISH="yarn publish parsegraph-method-dev.tgz --no-git-tag-version --prepatch --preid "$run_number-$node_version" --tag=dev-$node_version"
+$PUBLISH && exit
 for try in `seq 1 3`; do
-    yarn publish parsegraph-TODO-PACKAGE-NAME-dev.tgz --no-git-tag-version --prepatch --preid "$run_number-$node_version" --tag=dev-$node_version && exit
-    echo "$try. Retrying..."
-    sleep 3
+    sleep $try
+    git reset HEAD^
+    git checkout package.json
+    $PUBLISH && exit
 done
 exit 1
