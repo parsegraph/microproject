554f7fdc2d4901de174ab95a39831aba76925272
Aaron Faanes <dafrito@gmail.com>
2022-02-13 01:21:27 -0600

    Tag with node version

--- publish.sh
+++ publish.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+run_number=$1
+node_version=$2
+
 for try in `seq 1 3`; do
-    yarn publish parsegraph-TODO-PACKAGE-NAME-dev.tgz --no-git-tag-version --prepatch --preid "$*" --tag=dev && exit
+    yarn publish parsegraph-TODO-PACKAGE-NAME-dev.tgz --no-git-tag-version --prepatch --preid "$run_number-$node_version" --tag=dev-$node_version && exit
     echo "$try. Retrying..."
     sleep 3
 done
