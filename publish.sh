1ab1d9eff8b8552320f59e9e1a5bd1ab0b6afb2f
Aaron Faanes <dafrito@gmail.com>
2022-02-13 01:10:39 -0600

    Fix syntax error

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for `seq 1 3`; do
+for try in `seq 1 3`; do
     yarn publish parsegraph-TODO-PACKAGE-NAME-dev.tgz --no-git-tag-version --prepatch --preid "$*" && break
-    echo "Retrying..."
+    echo "$try. Retrying..."
     sleep 3
 done
 
