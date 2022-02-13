e4d85720bf59ffdab169782558727c694947aed8
Aaron Faanes <dafrito@gmail.com>
2022-02-13 01:15:57 -0600

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for try in `seq 1 3`; do
-    yarn publish parsegraph-TODO-PACKAGE-NAME-dev.tgz --no-git-tag-version --prepatch --preid "$*" && break
+    yarn publish parsegraph-TODO-PACKAGE-NAME-dev.tgz --no-git-tag-version --prepatch --preid "$*" && exit
     echo "$try. Retrying..."
     sleep 3
 done
-
+exit 1
