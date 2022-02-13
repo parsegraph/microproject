927665b905ac5b767847e02bd5ccd2df95a36072
Aaron Faanes <dafrito@gmail.com>
2022-02-13 13:04:05 -0600

    Automate publish

---
+++ publish-prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PUBLISH="yarn publish parsegraph-method-prod.tgz --no-git-tag-version --patch
+$PUBLISH && exit
+for try in `seq 1 3`; do
+    sleep $try
+    git checkout package.json
+    $PUBLISH && exit
+done
+exit 1
