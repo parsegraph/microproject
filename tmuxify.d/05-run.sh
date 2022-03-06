436428943c473f9409cbcc2c76b7750bd60d35d2
Aaron Faanes <dafrito@gmail.com>
2022-03-06 07:27:07 -0600

    Make demo restart on change

--- tmuxify.d/05-run.sh
+++ tmuxify.d/05-run.sh
@@ -3,4 +3,12 @@ if test $# -gt 0; then
     SITE_PORT=$1
     shift
 fi
-make demo SITE_PORT=$SITE_PORT
+while true; do
+    make demo SITE_PORT=$SITE_PORT &
+    serverpid=$!
+    trap 'kill -TERM $serverpid' TERM INT
+    sleep 0.2
+    inotifywait -e modify -r demo
+    kill -TERM $serverpid
+    sleep 0.2
+done
